08f03df11ac69e4baa5d2157c824567bf711c6c3
GeonwooVincentKim <kdsnop@gmail.com>
2021-10-01 18:00:27 +0900

    [Feature] - Testing

--- app/src/main/java/com/example/horizontalscrollview/MainActivity.kt
+++ app/src/main/java/com/example/horizontalscrollview/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.horizontalscrollview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ScrollView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val resultText: TextView by lazy {
+        findViewById(R.id.resultText)
+    }
 
+//    private val horizontalScrollViewUI: ScrollView by lazy{
+//        findViewById(R.id.horizontalScrollViewUI)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultText
+
+
+
+//        val padding: Int = ScreenUtils
     }
 }
